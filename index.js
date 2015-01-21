729cf766eaaaf98e2d420624a4023f6779124864
Steve Purdy <sfpurdy@mitre.org>
2015-01-21 15:40:09 -0500

    Fixed Directory

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ var express = require('express');
 var app = express();
 
 app.use(nginy);
-process.env['DIRECTORY'] = './public';
-console.log('DIRECTORY =', process.env.DIRECTORY)
-console.log('CWD:',process.cwd());
+
 app.listen(process.env.PORT || 3000);
+
 console.log('NGINY App Started');
