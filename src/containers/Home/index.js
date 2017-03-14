f8631cdad8aed5321cf2cffa0b9df6aa2a5c0d7c
Michael Stanaland <mstanaland@mac.com>
2017-03-14 07:29:53 -0400

    initial commit

---
+++ src/containers/Home/index.js
@@ -0,0 +1,39 @@
+
+import React from 'react';
+import Alert from '../../lib/components/Alert';
+import styles from './styles.scss';
+
+function Home() {
+  const message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.';
+  const longMessage = (
+    <div>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque.</p>
+      <p>Not what you expected? <a href="#Back">Go back.</a></p>
+    </div>);
+
+  return (
+    <section>
+      <p className={styles.paragraph}>
+        Welcome to the <strong>Universal React Starter-kyt</strong>.
+        This starter kyt should serve as the base for an advanced,
+        server-rendered React app.
+      </p>
+      <p className={styles.paragraph}>
+        Check out the Tools section for an outline of the libraries that
+        are used in this Starter-kyt.
+      </p>
+      <Alert
+        type="info"
+        title="Alert Title"
+        body={message}
+      />
+      <Alert
+        type="warning"
+        title="Alert Title"
+        body={longMessage}
+      />
+    </section>
+  );
+}
+
+export default Home;
